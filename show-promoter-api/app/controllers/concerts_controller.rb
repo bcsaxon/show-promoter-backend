43cae23c8feb583baaf47d101b64ddea0dfb6e92
Brett Saxon <brettcsaxon@gmail.com>
2020-02-26 17:50:28 -0500

    added nokogiri and controller actions

--- show-promoter-api/app/controllers/concerts_controller.rb
+++ show-promoter-api/app/controllers/concerts_controller.rb
@@ -1,20 +1,44 @@
 class ConcertsController < ApplicationController
-
+  
     def index
       concerts = Concert.all 
       render json: concerts
     end
+
+    def create
+      concert = Concert.create(concert_params)
+
+       if concert.valid?
+         render json: concert
+       else
+         render json: {errors: concert.errors.full_messages}
+        end
+     end
 
     def show 
       concert = Concert.find(params[:id])
       render json: concert  
     end
 
+    def update
+      concert = Concert.find(params[:id])
+      concert.update(concert_params)
+      render json: concert
+    end
+
+    def destroy
+      concert = Concert.find(params[:id])
+      concert.destroy
+    render json: {}
+    end
+
+    private
+
+    def concert_params
+      params.permit(:musician_name, :date, :venue, :img_url, :cost)
+
+    end
 
 
 end
 
-
-# class ReviewSerializer < ActiveModel::Serializer
-#   attributes :id, :title, :content, :drink_id, :user_id
-# end
--- show-promoter-api/app/serializers/concert_serializer.rb
+++ show-promoter-api/app/serializers/concert_serializer.rb
@@ -1,3 +1,4 @@
 class ConcertSerializer < ActiveModel::Serializer
   attributes :id, :musician_name, :date, :venue, :img_url, :cost
 end
+
